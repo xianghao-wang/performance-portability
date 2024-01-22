9219ddfd5347ea508b8c769e597f2cf24dccd1c8
Josh Milthorpe <josh.milthorpe@gmail.com>
2024-01-21 22:36:25 -0500

    build and run Chapel on V100, A100

--- benchmarking/2024/babelstream/a100-excl/benchmark.sh
+++ benchmarking/2024/babelstream/a100-excl/benchmark.sh
@@ -7,10 +7,12 @@ function usage() {
   echo "Usage: ./benchmark.sh build|run [COMPILER] [MODEL]"
   echo
   echo "Valid compilers:"
+  echo "  chapel-1.33"
   echo "  nvhpc-23.9"
   echo "  clang-17.0.6"
   echo
   echo "Valid models:"
+  echo "  chapel"
   echo "  kokkos"
   echo "  cuda"
   echo "  omp"
@@ -36,31 +38,42 @@ handle_cmd "${1}" "${2}" "${3}" "babelstream" "a100"
 
 export USE_MAKE=false
 
-append_opts "-DCMAKE_VERBOSE_MAKEFILE=ON"
-
 case "$COMPILER" in
+chapel-1.33)
+  load_nvhpc 23.11 # Chapel GPU also requires CUDA libraries
+  export CC=`which gcc` # the nvhpc module sets CC=nvc, which confuses Chapel
+  export CXX=`which g++` # the nvhpc module sets CXX=nvc++
+  export CUDA_PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/11.8
+  export PATH=${CUDA_PATH}/bin:$PATH
+  export CHPL_CUDA_PATH=$CUDA_PATH
+  source /noback/46x/chapel-1.33/util/setchplenv.bash
+  USE_MAKE=true
+  ;;
 nvhpc-23.9)
   load_nvhpc
   append_opts "-DCMAKE_C_COMPILER=$NVHPC_ROOT/compilers/bin/nvc"
   append_opts "-DCMAKE_CXX_COMPILER=$NVHPC_ROOT/compilers/bin/nvc++"
   ;;
 clang-17.0.6)
+  load_nvhpc 23.11 # ExCL OpenMP offload also requires CUDA libraries
   module load llvm/17.0.6-mpoffload
   append_opts "-DCMAKE_CXX_COMPILER=/usr/local/llvm/17.0.6/bin/clang++"
   ;;
-chapel-1.33)
-  export CUDA_PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/11.8
-  export CHPL_CUDA_PATH=$CUDA_PATH
-  ;;
 *) unknown_compiler ;;
 esac
+
+if [ "$USE_MAKE" = false ]; then
+  append_opts "-DCMAKE_VERBOSE_MAKEFILE=ON"
+fi
 
 fetch_src
 
 case "$MODEL" in
 chapel)
-  append_opts "-DCHPL_GPU=nvidia"
-  append_opts "-DCHPL_GPU_ARCH=sm_80"
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_PATH/lib64
+  append_opts "CHPL_LOCALE_MODEL=gpu"
+  append_opts "CHPL_GPU=nvidia"
+  append_opts "CHPL_GPU_ARCH=sm_80"
   BENCHMARK_EXE="chapel-stream"
   ;;
 kokkos)
