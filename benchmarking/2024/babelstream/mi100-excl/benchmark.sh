06019a8a7931aee166a2b3d8f55293e2cd6701a4
Josh Milthorpe <josh.milthorpe@gmail.com>
2024-01-23 06:30:03 -0500

    ExCL AMD systems

---
+++ benchmarking/2024/babelstream/mi100-excl/benchmark.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function usage() {
+  echo
+  echo "Usage: ./benchmark.sh build|run [COMPILER] [MODEL]"
+  echo
+  echo "Valid compilers:"
+  echo "  chapel-1.33"
+  echo "  rocm-5.4.3"
+  echo "  aomp-18.0.0"
+  echo
+  echo "Valid models:"
+  echo "  chapel"
+  echo "  kokkos"
+  echo "  hip"
+  echo "  omp"
+  echo
+}
+
+# Process arguments
+if [ $# -lt 3 ]; then
+  usage
+  exit 1
+fi
+
+SCRIPT_DIR=$(realpath "$(dirname "$(realpath "$0")")")
+source "${SCRIPT_DIR}/../../common.sh"
+source "${SCRIPT_DIR}/../fetch_src.sh"
+
+module load cmake/3.26.3
+
+handle_cmd "${1}" "${2}" "${3}" "babelstream" "mi100"
+
+export USE_MAKE=false
+
+case "$COMPILER" in
+chapel-1.33)
+  module load rocm/5.4.3 # Chapel GPU also requires ROCM
+  source /noback/46x/chapel-1.33/util/setchplenv.bash
+  USE_MAKE=true
+  ;;
+rocm-5.4.3)
+  module load rocm/5.4.3
+  export PATH="/opt/rocm-5.4.3/bin:${PATH:-}"
+  export ROCM_PATH="/opt/rocm-5.4.3"
+  ;;
+aomp-18.0.0)
+  module load rocm/5.4.3
+  export AOMP=$HOME/usr/lib/aomp_18.0-0
+  export PATH="$AOMP/bin:${PATH:-}"
+  export LD_LIBRARY_PATH="$AOMP/lib64:${LD_LIBRARY_PATH:-}"
+  export LIBRARY_PATH="$AOMP/lib64:${LIBRARY_PATH:-}"
+  export C_INCLUDE_PATH="$AOMP/include:${C_INCLUDE_PATH:-}"
+  export CPLUS_INCLUDE_PATH="$AOMP/include:${CPLUS_INCLUDE_PATH:-}"
+  ;;
+*) unknown_compiler ;;
+esac
+
+if [ "$USE_MAKE" = false ]; then
+  append_opts "-DCMAKE_VERBOSE_MAKEFILE=ON"
+fi
+
+fetch_src
+
+case "$MODEL" in
+chapel)
+  append_opts "CHPL_LOCALE_MODEL=gpu"
+  append_opts "CHPL_GPU=amd"
+  append_opts "CHPL_GPU_ARCH=gfx908"
+  BENCHMARK_EXE="chapel-stream"
+  ;;
+kokkos)
+  prime_kokkos
+  append_opts "-DMODEL=kokkos"
+  append_opts "-DKOKKOS_IN_TREE=$KOKKOS_DIR -DKokkos_ENABLE_HIP=ON"
+  append_opts "-DKokkos_ARCH_VEGA908=ON"
+  append_opts "-DCMAKE_C_COMPILER=gcc"
+  append_opts "-DCMAKE_CXX_COMPILER=hipcc"
+  BENCHMARK_EXE="kokkos-stream"
+  ;;
+hip)
+  append_opts "-DMODEL=hip"
+  append_opts "-DCMAKE_C_COMPILER=gcc"
+  append_opts "-DCMAKE_CXX_COMPILER=hipcc" # auto detected
+  append_opts "-DCXX_EXTRA_FLAGS=--offload-arch=gfx908"
+  BENCHMARK_EXE="hip-stream"
+  ;;
+omp)
+  append_opts "-DCMAKE_CXX_COMPILER=/usr/lib/aomp_18.0-0/bin/amdclang++"
+  append_opts "-DMODEL=omp"
+  append_opts "-DOFFLOAD=AMD:gfx908"
+  BENCHMARK_EXE="omp-stream"
+  ;;
+*) unknown_model ;;
+esac
+
+handle_exec
---
+++ benchmarking/2024/babelstream/rome-excl/benchmark.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function usage() {
+  echo
+  echo "Usage: ./benchmark.sh build|run [COMPILER] [MODEL]"
+  echo
+  echo "Valid compilers:"
+  echo "  chapel-1.33"
+  echo "  clang-17.0.6"
+  echo
+  echo "Valid models:"
+  echo "  chapel"
+  echo "  kokkos"
+  echo "  omp"
+  echo "  std-data"
+  echo "  std-indices"
+  echo "  std-ranges"
+  echo
+}
+
+# Process arguments
+if [ $# -lt 3 ]; then
+  usage
+  exit 1
+fi
+
+SCRIPT_DIR=$(realpath "$(dirname "$(realpath "$0")")")
+source "${SCRIPT_DIR}/../../common.sh"
+source "${SCRIPT_DIR}/../fetch_src.sh"
+
+module load cmake/3.26.3
+
+handle_cmd "${1}" "${2}" "${3}" "babelstream" "rome"
+
+export USE_MAKE=false
+
+case "$COMPILER" in
+chapel-1.33)
+  source /noback/46x/chapel-1.33/util/setchplenv.bash
+  USE_MAKE=true
+  ;;
+clang-17.0.6)
+  module load llvm/17.0.6
+  append_opts "-DCMAKE_CXX_COMPILER=/auto/software/swtree/ubuntu22.04/x86_64/llvm/17.0.6/bin/clang++"
+  ;;
+*) unknown_compiler ;;
+esac
+
+if [ "$USE_MAKE" = false ]; then
+  append_opts "-DCMAKE_VERBOSE_MAKEFILE=ON"
+fi
+
+fetch_src
+
+case "$MODEL" in
+chapel)
+  BENCHMARK_EXE="chapel-stream"
+  append_opts "CHPL_LOCALE_MODEL=flat"
+  ;;
+kokkos)
+  prime_kokkos
+  append_opts "-DMODEL=kokkos"
+  append_opts "-DKOKKOS_IN_TREE=$KOKKOS_DIR -DKokkos_ENABLE_OPENMP=ON"
+  append_opts "-DKokkos_ARCH_ZEN2=ON"
+  BENCHMARK_EXE="kokkos-stream"
+  ;;
+omp)
+  append_opts "-DMODEL=omp"
+  BENCHMARK_EXE="omp-stream"
+  ;;
+std-data)
+  append_opts "-DMODEL=std-data"
+  BENCHMARK_EXE="std-data-stream"
+  ;;
+std-indices)
+  append_opts "-DMODEL=std-indices"
+  BENCHMARK_EXE="std-indices-stream"
+  ;;
+std-ranges)
+  append_opts "-DMODEL=std-ranges"
+  BENCHMARK_EXE="std-ranges-stream"
+  ;;
+*) unknown_model ;;
+esac
+
+handle_exec
